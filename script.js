90a1fb86d1bc48ba14e4b9afcbf39e9e0b1a164f
thiagoo23114 <thiagobancayanbecerra@gmail.com>
2026-05-23 18:16:33 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 let primerNumero = "";
+let segundoNumero= "";
 let operadorActual = "";
+
 
 function elegirOperador(operador) {
     primerNumero = pantalla.value;
     operadorActual = operador;
-    pantalla.value = "0"
+    pantalla.value = pantalla.value + " " + operador + " ";
 
 }
 const pantalla =
@@ -18,3 +20,40 @@ function agregarNumero(numero) {
             pantalla.value + numero;
     }
 }
+function calcular() {
+    let pedazos = pantalla.value.split(" ");
+    let num1 = parseFloat(pedazos[0]);
+    let num2 = parseFloat(pedazos[2]);
+    let resultado = 0;
+
+    if (pedazos[1] === "+"){
+        resultado = num1 + num2;
+    }
+
+    if (pedazos[1] === "-"){
+    resultado = num1 - num2;
+    }
+
+    if (pedazos[1] === "*"){
+        resultado = num1 * num2;
+    }
+
+if (pedazos[1] === "/"){
+        resultado = num1 / num2;
+    }
+
+    pantalla.value = resultado;
+}
+function borrarTodo() {
+    pantalla.value = "0";
+}
+function borrarUno() {
+    if (pantalla.value !== "0" && pantalla.value !== "") {
+        pantalla.value = pantalla.value.slice(0, -1);
+    }
+    if (pantalla.value === "") {
+        pantalla.value = "0";
+    }
+}
+
+
